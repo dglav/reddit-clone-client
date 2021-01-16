19181ffda6486173aa6b842dbab10642fd27c27d
Drew <glavich_drew@yahoo.com>
2021-01-16 18:12:15 +0900

    feat: add get Posts Urql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>Hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>Hello world</div>
+      <br></br>
+      {data?.posts.map((post) => {
+        return <div key={post.id}>{post.title}</div>;
+      })}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
