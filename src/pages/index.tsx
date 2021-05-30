219e481b4b63a41ac3247ec097aa1d70d1200503
Drew <glavich_drew@yahoo.com>
2021-05-30 20:27:54 +0900

    feat: shows loading for when new posts are being fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
@@ -15,7 +15,11 @@ import Layout from "../components/Layout";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data, fetching }] = usePostsQuery({ variables: { limit: 10 } });
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
+  });
+  const [{ data, fetching, stale }] = usePostsQuery({ variables });
 
   if (!fetching && !data) {
     return <div>you got query failed for some reason</div>;
@@ -34,7 +38,7 @@ const Index = () => {
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.map((post) => {
+          {data?.posts?.posts?.map((post) => {
             return (
               <Box key={post.id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{post.title}</Heading>
@@ -44,11 +48,23 @@ const Index = () => {
           })}
         </Stack>
       )}
-      <Flex justifyContent="center">
-        <Button isLoading={fetching} my={8}>
-          Load More
-        </Button>
-      </Flex>
+      {data && data.posts.hasMore ? (
+        <Flex justifyContent="center">
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor:
+                  data?.posts.posts[data?.posts.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching || stale}
+            my={8}
+          >
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
